98de753484baade509682c34a5772b139f5f2c3b
Oleg Ssorin <helgius@gmail.com>
2012-04-04 23:42:08 +0300

    missed Math

---
+++ Math.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "stdint.h"
+
+long map_l(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+uint8_t map_b(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
