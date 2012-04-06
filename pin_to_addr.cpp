9115fd376e1333d9835e7ee81d499b66f91c6850
Oleg Ssorin <helgius@gmail.com>
2012-04-06 19:02:41 +0300

    Totaly reworked|optimized pin_to_hardware

--- pin_to_addr.cpp
+++ pin_to_addr.cpp
@@ -8,32 +8,30 @@
 
 #include "pin_to_addr.h"
 
-inline PIN_HW_INFORMATION get_pin_information(uint8_t pin_hw_number)
-{
-	PIN_HW_INFORMATION tmp;
-	tmp.PINx 		= pgm_read_word(PIN_2_ADDRESS[pin_hw_number]);
-	tmp.DDRx 		= tmp.PINx+1;
-	tmp.PORTx 		= tmp.PINx+2;
-	tmp.PIN_mask	= pgm_read_byte(PIN_2_MASK[pin_hw_number]);
-
-	return tmp;
-}
-
 void set_DDR(uint8_t pin_hw_number, io_enum mode)
 {
-	PIN_HW_INFORMATION tmp = get_pin_information(pin_hw_number);
+	volatile uint8_t* DDR = (volatile uint8_t*) pgm_read_word(PIN_2_ADDRESS+pin_hw_number)+1;
+	uint8_t mask = pgm_read_byte(PIN_2_MASK+pin_hw_number);
 
-	*((volatile uint8_t*) (tmp.DDRx)) = (*((volatile uint8_t*) tmp.DDRx) & tmp.PIN_mask) | (mode?tmp.PIN_mask:0);
+	if (mode==INPUT)
+		*DDR &= ~mask;
+	else
+		*DDR |= mask;
 }
 
 void set_PORT(uint8_t pin_hw_number, port_state_enum mode){
-	PIN_HW_INFORMATION tmp = get_pin_information(pin_hw_number);
+	volatile uint8_t* PORT = (volatile uint8_t*) pgm_read_word(PIN_2_ADDRESS+pin_hw_number)+2;
+	uint8_t mask = pgm_read_byte(PIN_2_MASK+pin_hw_number);
 
-	*((volatile uint8_t*) (tmp.PORTx)) = (*((volatile uint8_t*) tmp.PORTx) & tmp.PIN_mask) | (mode?tmp.PIN_mask:0);
+	if (mode==LOW)
+		*PORT &= ~mask;
+	else
+		*PORT |= mask;
 }
 
 uint8_t get_PIN(uint8_t pin_hw_number){
-	PIN_HW_INFORMATION tmp = get_pin_information(pin_hw_number);
+	volatile uint8_t* PIN = (volatile uint8_t*) pgm_read_word(PIN_2_ADDRESS+pin_hw_number);
+	uint8_t mask = pgm_read_byte(PIN_2_MASK+pin_hw_number);
 
-	return *((volatile uint8_t*) (tmp.PINx)) & tmp.PIN_mask;
+	return *PIN & mask;
 }
--- pin_to_addr.h
+++ pin_to_addr.h
@@ -10,13 +10,17 @@
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
+#include <util/delay.h>
 
 struct PIN_HW_INFORMATION {uint16_t PINx; uint16_t DDRx; uint16_t PORTx; uint8_t PIN_mask;};
 typedef enum {INP_MODE=0, OUT_MODE} io_enum;
 typedef enum {STATE_LOW=0, STATE_HIGH} port_state_enum;
 
-#define LOW 0
-#define HIGH 1
+#define LOW (port_state_enum) 0
+#define HIGH (port_state_enum) 1
+
+#define INPUT (io_enum) 0
+#define OUTPUT (io_enum) 1
 
 #if defined (__AVR_ATmega640__) || defined (__AVR_ATmega1280__) || defined (__AVR_ATmega2560__)
 
@@ -55,6 +59,7 @@ const uint16_t PROGMEM PIN_2_ADDRESS[] = {
 	};
 
 const uint8_t PROGMEM PIN_2_MASK[] = {
+		0,
 		_BV(4),															//PORTG
 		_BV(0), _BV(1), _BV(2), _BV(3), _BV(4), _BV(5), _BV(6), _BV(7), //PORTE
 		0,0, //VCC, GND
@@ -117,7 +122,6 @@ const uint8_t PROGMEM PIN_2_MASK[] = {
 
 #endif //ATmega640, ATmega1280, ATmega2560
 
-PIN_HW_INFORMATION get_pin_information(uint8_t pin_hw_number);
 void set_DDR(uint8_t pin_hw_number, io_enum mode);
 void set_PORT(uint8_t pin_hw_number, port_state_enum mode);
 uint8_t get_PIN(uint8_t pin_hw_number);
